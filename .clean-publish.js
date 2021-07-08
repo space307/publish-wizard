517ec9155333c456b6d34a896055815e9aff356b
Viktor <v.pasynok@space307.com>
2021-07-08 18:43:13 +0300

    maintain: using clean-publish

---
+++ .clean-publish.js
@@ -0,0 +1,15 @@
+module.exports = {
+  files: [
+    '.github',
+    '.lefthook',
+    '.vscode',
+    '.editorconfig',
+    '.gitignore',
+    '.prettierignore',
+    '.prettierrc',
+    'lefthook.yml',
+  ],
+  fields: ['devDependencies', 'scripts', 'keywords'],
+  packageManager: 'npm',
+  access: 'public',
+};
